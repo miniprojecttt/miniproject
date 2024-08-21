77cd41c0a09c34948d8e333ac20de9bc8d1edcf3
miniprojecttt <bhargavsaihh72@gmail.com>
2024-08-21 19:42:52 +0530

    Update grammer_corrector.py

--- mini project/grammer_corrector.py
+++ mini project/grammer_corrector.py
@@ -4,12 +4,9 @@ from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 def make_good(text):
     try:
-        # api 1: AIzaSyBNbNNUCp_sEBz2luE9m-8-UXLgZ9R7Vlo
-        # api 2: AIzaSyDcEQhNMWZLiAfISxumMJaUTGf5YhK8XAI
-        # api 3: AIzaSyAs83PgRKmJyLFBbenPFJSoWk6n_ktkbUc
-
+        api_key = ""
         text = f"Summarize the reviews\n{text}"
-        genai.configure(api_key="AIzaSyBNbNNUCp_sEBz2luE9m-8-UXLgZ9R7Vlo")
+        genai.configure(api_key=api_key)
         safety_settings = {
             HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
             HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
